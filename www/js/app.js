3f88f9b811ac5a1d77adfb553e586fd0859d64a7
Alan Kligman <alan.kligman@gmail.com>
2012-07-05 15:08:13 -0400

    Moved example game over to www.

--- www/js/app.js
+++ www/js/app.js
@@ -1,62 +1,128 @@
+// Setup requestAnimationFrame
+requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||  
+                        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-// The code below uses require.js, a module system for javscript:
-// http://requirejs.org/docs/api.html#define
-//
-// You don't have to use require.js, and you can delete all of this if
-// you aren't (make sure to uncomment the script tags in index.html also)
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
 
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "img/background.png";
 
-// Set the path to jQuery, which will fall back to the local version
-// if google is down
-require.config({
-    paths: {'jquery':
-            ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-             'lib/jquery']}
-});
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "img/hero.png";
 
-var global = this;
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "img/monster.png";
 
-require(['jquery'], function($) {
-  // Create the canvas
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = 512;
-  canvas.height = 480;
-  document.body.appendChild(canvas);
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+var monstersCaught = 0;
 
-//Reset game to original state
-  function reset() {
+// Handle keyboard controls
+var keysDown = {};
 
-  };
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
 
 // Update game objects
-  function update( modifier ) {
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
 
-  };
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
 
 // Draw everything
-  function render() {
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
 
-  };
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+};
 
 // The main game loop
-  var main = function () {
-    var now = Date.now();
-    var delta = now - then;
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
 
-    update(delta / 1000);
-    render();
+	update(delta / 1000);
+	render();
 
-    then = now;
-  };
+	then = now;
+	requestAnimationFrame(main);
+};
 
 // Let's play this game!
-  reset();
-  var then = Date.now();
-  // Execute as fast as possible
-  setInterval(main, 1);
-
-});
-
-// END REQUIRE.JS CODE
-// Remove all of this if not using require.js
+reset();
+var then = Date.now();
+main();
